9ee8fb8d87df597c75a8fa194b2a03891e5d2d91
minatore <>
2019-08-16 02:13:23 +0200

    aggiunto basi comando warn

--- bot.py
+++ bot.py
@@ -14,18 +14,38 @@ client = discord.Client()
 
 
 @client.event
-def on_message(message):
+async def on_message(message):
     message_ar = message.content.split(" ");
     cmd = message_ar[0];
-    args = message_ar.slice(1);
-    argresult = args.join(' ');
+    args = message_ar[1:len(message_ar)];
+    argresult = ' '.join(args);
     prefix = '\\'
 
     if cmd == prefix + "ping":
-        message.reply("pong");
+        await message.channel.send("pong");
 
     if cmd == prefix + "warn":
-        message.reply("ciao");
+        try:
+            target = message.mentions[0].id;
+        except IndexError:
+            try:
+                target = args[0];
+                if len(target) != 18:
+                    await message.channel.send("ID non valido")
+                    return;
+            except IndexError:
+                await message.channel.send("Devi taggare qualcuno")
+                return;
+
+        motivo = ' '.join(message_ar[2:len(message_ar)])
+        if motivo == '':
+            await message.channel.send("Devi specificare un motivo")
+            return;
+
+        with open("warn.txt", "a") as f:
+            f.write(str(target)+" "+motivo+"\n")
+
+        await message.channel.send("Warn loggato")
 
 
 client.run(token);
