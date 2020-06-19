5592e1f49f9845b14772e269c1e30bf4d99a1354
minatore <>
2020-06-19 19:18:31 +0200

    fix permissionlevel

--- bot.py
+++ bot.py
@@ -65,7 +65,7 @@ async def on_message(message):
         role_s = discord.utils.find(lambda r: r.name == 'Supervisore', message.guild.roles)
         role_m = discord.utils.find(lambda r: r.name == 'Moderatore', message.guild.roles)
         role_h = discord.utils.find(lambda r: r.name == 'Helper', message.guild.roles)
-        await time.sleep(0.1)
+        await asyncio.sleep(0.1)
         if role_a in message.guild.get_member(message.author.id).roles:
             permissionlevel = 5
         else:
@@ -297,7 +297,7 @@ async def on_raw_reaction_add(payload):
             role_s = discord.utils.find(lambda r: r.name == 'Supervisore', canale.guild.roles)
             role_m = discord.utils.find(lambda r: r.name == 'Moderatore', canale.guild.roles)
             role_h = discord.utils.find(lambda r: r.name == 'Helper', canale.guild.roles)
-            await time.sleep(0.1)
+            await asyncio.sleep(0.1)
             if role_a in client.get_guild(681624606976901211).get_member(user.id).roles:
                 permissionlevel = 5
             else:
